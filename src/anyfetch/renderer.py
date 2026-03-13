2c3c049fbd52cbec084ec93599c482f256bb7a48
envcat <266045439+envcat@users.noreply.github.com>
2026-03-14 06:29:38 +0800

    feat: create Renderer class to combine logo and info rendering

---
+++ src/anyfetch/renderer.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+
+from anyfetch.info.renderer import InfoRenderer
+from anyfetch.layout.base import LayoutStrategy
+from anyfetch.logo.renderer import LogoRenderer
+
+
+@dataclass
+class Renderer:
+    logo_renderer: LogoRenderer
+    info_renderer: InfoRenderer
+    layout: LayoutStrategy
+
+    def render(self):
+        logo = self.logo_renderer.render()
+        info = self.info_renderer.render()
+        lines = self.layout.combine(logo, info)
+        for line in lines:
+            print(line)
