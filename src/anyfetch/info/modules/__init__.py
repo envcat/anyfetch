0441611bc262abcdcad812e03eb889804106682c
envcat <266045439+envcat@users.noreply.github.com>
2026-03-15 04:35:18 +0800

    feat: add built-in info modules registration system

---
+++ src/anyfetch/info/modules/__init__.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from anyfetch.info.modules.base import InfoModule
+from anyfetch.info.modules.cpu import CPUInfo
+from anyfetch.info.modules.disk import DiskInfo
+from anyfetch.info.modules.kernal import KernalInfo
+from anyfetch.info.modules.memory import MemoryInfo
+from anyfetch.info.modules.os import OSInfo
+from anyfetch.info.modules.shell import ShellInfo
+from anyfetch.info.modules.uptime import UptimeInfo
+from anyfetch.info.modules.user import UserInfo
+
+BUILTIN_MODULES: list[type[InfoModule]] = [
+    UserInfo,
+    CPUInfo,
+    MemoryInfo,
+    DiskInfo,
+    UptimeInfo,
+    ShellInfo,
+    OSInfo,
+    KernalInfo,
+]
+
+
+if TYPE_CHECKING:
+    from anyfetch.info.registry import InfoRegistry
+
+
+def register_builtin_modules(registry: type[InfoRegistry]) -> None:
+    for module_cls in BUILTIN_MODULES:
+        registry.register(module_cls)
+
+
+__all__ = [
+    "InfoModule",
+    "BUILTIN_MODULES",
+    "register_builtin_modules",
+    "UserInfo",
+    "CPUInfo",
+    "MemoryInfo",
+    "DiskInfo",
+    "UptimeInfo",
+    "ShellInfo",
+    "OSInfo",
+    "KernalInfo",
+]
