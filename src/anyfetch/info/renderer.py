116f04a4ec90a88422e41987961aa02193d53de7
envcat <266045439+envcat@users.noreply.github.com>
2026-03-15 01:56:51 +0800

    refactor: update InfoRenderer to use typed list for modules and config

--- src/anyfetch/info/renderer.py
+++ src/anyfetch/info/renderer.py
@@ -2,19 +2,20 @@ from dataclasses import dataclass
 
 from anyfetch.color.colorizer import InfoColorizer
 from anyfetch.config import InfoConfig
+from anyfetch.info.modules.base import InfoModule
 
 
 @dataclass
 class InfoRenderer:
-    modules: list
-    cfg: InfoConfig
+    modules: list[InfoModule]
+    config: InfoConfig
     colorizer: InfoColorizer
 
-    def render(self) -> list:
+    def render(self) -> list[str]:
         lines = []
         for module in self.modules:
             value = module.fetch()
             key = self.colorizer.color_key(module.key)
-            sep = self.colorizer.color_separator(self.cfg.separator)
+            sep = self.colorizer.color_separator(self.config.separator)
             lines.append(f"{key}{sep}{value}")
         return lines
