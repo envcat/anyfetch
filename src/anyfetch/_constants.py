5384d3090ae70d56dbc7bd152641b49bdf603ddf
envcat <266045439+envcat@users.noreply.github.com>
2026-03-15 05:12:45 +0800

    refactor: rename variable for clarity in configuration file path setup

--- src/anyfetch/_constants.py
+++ src/anyfetch/_constants.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 from platformdirs import PlatformDirs
 
-dir = PlatformDirs("anyfetch", "envcat")
-CONFIG_FILE_PATH = Path(dir.user_config_dir, "anyfetch.toml")
+platform_dir = PlatformDirs("anyfetch", "envcat")
+CONFIG_FILE_PATH = Path(platform_dir.user_config_dir, "anyfetch.toml")
