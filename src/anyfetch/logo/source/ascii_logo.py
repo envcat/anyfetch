2e53c67772732460817a8d4ede07b6a819baa917
envcat <266045439+envcat@users.noreply.github.com>
2026-03-14 08:37:41 +0800

    feat: update mock ASCII distro logo.

--- src/anyfetch/logo/source/ascii_logo.py
+++ src/anyfetch/logo/source/ascii_logo.py
@@ -8,4 +8,13 @@ class AsciiLogo(LogoSourceStrategy):
     distro: str
 
     def render(self) -> list[str]:
-        return ["$1   /\\", "$2  /  \\", "$3 /____\\", f"$1 {self.distro}"]
+        return [
+            "$1   .--.",
+            "$1  |o_o |",
+            "$2  |:_/ |",
+            "$2 //   \\ \\",
+            "$3(|     | )",
+            "$3/'\\_   _/`\\",
+            "$1\\___)=(___/",
+            f"$1  {self.distro}",
+        ]
